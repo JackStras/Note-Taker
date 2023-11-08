6fa2d34f93c4e2848aa9102f3c38646e7af4113e
JackStras <jackstrasbaugh@yahoo.com>
2023-11-08 17:18:21 -0500

    Added required dependencies

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
+
 const PORT = 3001
 const app = express()
+
+app.use(express.static('public'))
